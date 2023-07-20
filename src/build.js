15d886480d1dbe481e49cf9b271a22975e83203a
Doug Silkstone <doug@withseismic.com>
2023-07-20 23:00:50 +0200

    Remember to ship your .js

---
+++ src/build.js
@@ -0,0 +1,27 @@
+const esbuild = require('esbuild')
+
+// Build the main application for Node.js
+esbuild
+    .build({
+        entryPoints: ['src/server.ts'],
+        bundle: true,
+        platform: 'node',
+        target: ['node16'],
+        outbase: 'src',  // base directory
+        outdir: 'dist',  // output directory
+    })
+    .then(() => {
+        // Build the user scripts for the browser
+        return esbuild.build({
+            entryPoints: ['src/userscripts/main.ts'],
+            bundle: true,
+            platform: 'browser',
+            target: ['es2020'],
+            outbase: 'src',
+            outdir: 'dist',
+        });
+    })
+    .catch(() => {
+        process.exit(1);
+    });
+
